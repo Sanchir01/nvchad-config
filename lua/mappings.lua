b53c48a037ddbe12708a54073883fa997fa0b117
sanchir <emgushovs@mail.ru>
2024-12-19 23:58:03 +0300

    update config

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,21 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+
+
+-- Git
+map("n", "<leader>gl", ":Flog<CR>", { desc = "Git Log" })
+map("n", "<leader>gf", ":DiffviewFileHistory<CR>", { desc = "Git File History" })
+map("n", "<leader>gc", ":DiffviewOpen HEAD~1<CR>", { desc = "Git Last Commit" })
+map("n", "<leader>gt", ":DiffviewToggleFile<CR>", { desc = "Git File History" })
+
+-- Trouble
+
+map("n", "<leader>qx", "<cmd>TroubleToggle<CR>", { desc = "Open Trouble" })
+map("n", "<leader>qw", "<cmd>TroubleToggle workspace_diagnostics<CR>", { desc = "Open Workspace Trouble" })
+map("n", "<leader>qd", "<cmd>TroubleToggle document_diagnostics<CR>", { desc = "Open Document Trouble" })
+map("n", "<leader>qq", "<cmd>TroubleToggle quickfix<CR>", { desc = "Open Quickfix" })
+map("n", "<leader>ql", "<cmd>TroubleToggle loclist<CR>", { desc = "Open Location List" })
+map("n", "<leader>qt", "<cmd>TodoTrouble<CR>", { desc = "Open Todo Trouble" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
